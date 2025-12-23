0f94f777348cbfa276c8f8d81cfe2b46cbac0ae4
safin_elahi <safinelahi@gmail.com>
2025-12-23 06:02:26 +0600

    some varcel Speed Insights add in project

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import Home from "./pages/Home/Home";
 import Layout from "./components/Layout";
 import SignUp from "./pages/SignUp/SignUp";
 import Login from "./pages/Login/Login";
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
 
 function App() {
   return (
-    <Routes>
-      {/* Option 1: Pages WITH Navbar & Footer (Wrapped in Layout) */}
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
 
-      {/* Option 2: Pages WITHOUT Navbar & Footer (Standalone) */}
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <SpeedInsights />
+    </>
   );
 }
 
